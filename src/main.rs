532432403a9c278b5d6008f5d2c487447c53dc44
Keony Schröer <keony.schroer@gmail.com>
2024-02-11 14:58:50 -0300

    fix(keyup): handle the keyup event

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ use windows::core::PCWSTR;
 use windows::Win32::Foundation::{HWND, LPARAM, LRESULT, WPARAM};
 use windows::Win32::UI::Input::KeyboardAndMouse::{
     RegisterHotKey, SendInput, UnregisterHotKey, INPUT, INPUT_0, INPUT_KEYBOARD, KEYBDINPUT,
-    KEYBD_EVENT_FLAGS, KEYEVENTF_KEYUP, MOD_ALT, MOD_WIN, VK_CAPITAL, VK_ESCAPE,
+    KEYBD_EVENT_FLAGS, KEYEVENTF_KEYUP, MOD_ALT, MOD_WIN, VIRTUAL_KEY, VK_CAPITAL, VK_ESCAPE,
 };
 use windows::Win32::UI::WindowsAndMessaging::{
-    CallNextHookEx, DispatchMessageW, GetMessageExtraInfo, GetMessageW, MessageBoxW,
-    SetWindowsHookExW, TranslateMessage, UnhookWindowsHookEx, HHOOK, KBDLLHOOKSTRUCT, MB_OK, MSG,
-    WH_KEYBOARD_LL, WM_HOTKEY, WM_KEYDOWN,
+    CallNextHookEx, DispatchMessageW, GetMessageW, MessageBoxW, SetWindowsHookExW,
+    TranslateMessage, UnhookWindowsHookEx, HHOOK, KBDLLHOOKSTRUCT, MB_OK, MSG, WH_KEYBOARD_LL,
+    WM_HOTKEY, WM_KEYDOWN, WM_KEYUP,
 };
 
 use windows::Win32::System::LibraryLoader::GetModuleHandleW;
@@ -18,91 +18,71 @@ static mut ACTIVE: bool = true;
 
 const CBSIZE: i32 = std::mem::size_of::<INPUT>() as i32;
 
-fn toggle_capslock() {
-    unsafe {
-        let caps_up = INPUT {
-            r#type: INPUT_KEYBOARD,
-            Anonymous: INPUT_0 {
-                ki: KEYBDINPUT {
-                    wVk: VK_CAPITAL,
-                    wScan: 0,
-                    dwFlags: KEYEVENTF_KEYUP,
-                    time: 0,
-                    dwExtraInfo: GetMessageExtraInfo().0 as usize,
-                },
+unsafe fn send_input(vk: VIRTUAL_KEY, flags: KEYBD_EVENT_FLAGS) {
+    let input = INPUT {
+        r#type: INPUT_KEYBOARD,
+        Anonymous: INPUT_0 {
+            ki: KEYBDINPUT {
+                wVk: vk,
+                wScan: 0,
+                dwFlags: flags,
+                time: 0,
+                dwExtraInfo: 0,
             },
-        };
-
-        let caps_down = INPUT {
-            r#type: INPUT_KEYBOARD,
-            Anonymous: INPUT_0 {
-                ki: KEYBDINPUT {
-                    wVk: VK_CAPITAL,
-                    wScan: 0,
-                    dwFlags: KEYBD_EVENT_FLAGS(0),
-                    time: 0,
-                    dwExtraInfo: GetMessageExtraInfo().0 as usize,
-                },
-            },
-        };
-
-        SendInput(&[caps_up, caps_down], CBSIZE);
-    }
+        },
+    };
+    SendInput(&[input], CBSIZE);
 }
 
 unsafe extern "system" fn keyboard_hook_proc(code: i32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
-    if ACTIVE {
-        if code >= 0 && wparam.0 as u32 == WM_KEYDOWN {
-            let kbd_struct = lparam.0 as *const KBDLLHOOKSTRUCT;
-            let vk_code = (*kbd_struct).vkCode;
+    if ACTIVE && code >= 0 {
+        let kbd_struct = lparam.0 as *const KBDLLHOOKSTRUCT;
+        let vk_code = (*kbd_struct).vkCode as u16;
+        let vk_code = VIRTUAL_KEY(vk_code);
 
-            if vk_code == VK_CAPITAL.0 as u32 {
-                let escape_press = INPUT {
-                    r#type: INPUT_KEYBOARD,
-                    Anonymous: INPUT_0 {
-                        ki: KEYBDINPUT {
-                            wVk: VK_ESCAPE,
-                            wScan: 0,
-                            dwFlags: KEYBD_EVENT_FLAGS(0),
-                            time: 0,
-                            dwExtraInfo: GetMessageExtraInfo().0 as usize,
-                        },
-                    },
-                };
-
-                SendInput(&[escape_press], CBSIZE);
-                return LRESULT(1);
-            } else if vk_code == VK_ESCAPE.0 as u32 {
-                toggle_capslock();
+        match vk_code {
+            VK_CAPITAL => {
+                if wparam.0 as u32 == WM_KEYDOWN {
+                    send_input(VK_ESCAPE, KEYBD_EVENT_FLAGS(0));
+                } else if wparam.0 as u32 == WM_KEYUP {
+                    send_input(VK_ESCAPE, KEYEVENTF_KEYUP);
+                }
                 return LRESULT(1);
             }
-        }
+            VK_ESCAPE => {
+                if wparam.0 as u32 == WM_KEYDOWN {
+                    send_input(VK_CAPITAL, KEYBD_EVENT_FLAGS(0));
+                } else if wparam.0 as u32 == WM_KEYUP {
+                    send_input(VK_CAPITAL, KEYEVENTF_KEYUP);
+                }
+                return LRESULT(1);
+            }
+            _ => {}
+        };
     }
 
     let null_hhook: HHOOK = HHOOK(std::ptr::null::<isize>() as isize);
     CallNextHookEx(null_hhook, code, wparam, lparam)
 }
 
-fn notification(hwnd: HWND) {
-    unsafe {
-        let title: Vec<u16> = "esc_to_caps".encode_utf16().chain(Some(0)).collect();
+unsafe fn notification(hwnd: HWND) {
+    let title: Vec<u16> = "esc_to_caps".encode_utf16().chain(Some(0)).collect();
 
-        let body: Vec<u16> = match ACTIVE {
-            true => "Capslock -> Esc\nEsc -> Capslock"
-                .encode_utf16()
-                .chain(Some(0))
-                .collect(),
-            false => "Capslock -> Capslock\nEsc -> Esc"
-                .encode_utf16()
-                .chain(Some(0))
-                .collect(),
-        };
+    let body: Vec<u16> = match ACTIVE {
+        true => "Capslock -> Esc\nEsc -> Capslock"
+            .encode_utf16()
+            .chain(Some(0))
+            .collect(),
+        false => "Capslock -> Capslock\nEsc -> Esc"
+            .encode_utf16()
+            .chain(Some(0))
+            .collect(),
+    };
 
-        let body_ptr = body.first().unwrap();
-        let title_ptr = title.first().unwrap();
+    let body_ptr = body.first().unwrap();
+    let title_ptr = title.first().unwrap();
 
-        MessageBoxW(hwnd, PCWSTR(body_ptr), PCWSTR(title_ptr), MB_OK);
-    }
+    MessageBoxW(hwnd, PCWSTR(body_ptr), PCWSTR(title_ptr), MB_OK);
 }
 
 fn main() {
